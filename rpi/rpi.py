33193752e48a51b740ebe68ff02694fc29007651
emile.legallic <emile.legallic@telecom-paris.fr>
2023-03-22 14:19:57 +0100

    Config RPI

--- rpi/rpi.py
+++ rpi/rpi.py
@@ -3,13 +3,15 @@ from time import sleep
 import requests
 
 REQUEST_DELAY = 2 #Le délais entre deux request au serveur
+OPEN_TIME = 10
+LOCKERS_URL = 'http://137.194.211.70/lockers'
 rl = LED(4)
 
 while(True):
-    r = requests.get('http://137.194.211.70/lockers')
+    r = requests.get(LOCKERS_URL)
     lockersState = list(r.json())
-    if int(lockersState[0]):
+    if lockersState[0]:
         rl.on()
-    else:
+        sleep(OPEN_TIME)
         rl.off()
     sleep(REQUEST_DELAY)
